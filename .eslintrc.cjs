ae0719e7ef94cf9d5846c5ef26a8c9e5e49f098f
Kevin Luis <k.luis360@gmail.com>
2025-12-19 13:31:52 -0300

    chore: setup vite react typescript lint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Prova Técnica Frontend</h1>;
+}
+
+export default App;
